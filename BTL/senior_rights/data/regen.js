66fc5bd4e6f1c031b68151c38c71031e59800255
yairron <yairron@gmail.com>
2025-12-18 11:14:16 +0200

    fix hachnasa in line 10 at rights-data.js

---
+++ BTL/senior_rights/data/regen.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('rights-data.json', 'utf8'));
+
+const jsContent = `// נתוני זכויות אזרחים ותיקים - עודכן אוטומטית
+// עדכון אחרון: ${new Date(data.lastUpdate).toLocaleString('he-IL')}
+
+const RIGHTS_DATA = ${JSON.stringify(data, null, 2)};
+
+// ייצוא לשימוש בדפדפן
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = RIGHTS_DATA;
+}
+`;
+
+fs.writeFileSync('rights-data.js', jsContent, 'utf8');
+console.log('✅ rights-data.js regenerated');
